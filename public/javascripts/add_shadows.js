9c298c339c33098bd819471f7068dd35e1cb904a
Tyler Lesmann <redhatcat@gmail.com>
2010-07-07 10:19:29 -0600

    setTimeout to fix bad shadows.

--- public/javascripts/add_shadows.js
+++ public/javascripts/add_shadows.js
@@ -1,7 +1,16 @@
-jQuery(document).ready(function(){
-  jQuery("div.card, .entry-body-html pre, .entry-body-html blockquote").dropShadow({
+function add_shadows() {
+  var targets = [
+    "div.card",
+    ".entry-body-html pre",
+    ".entry-body-html blockquote"
+  ];
+  jQuery(targets.join(', ')).dropShadow({
     left: 4,
     top: 4,
     opacity: 0.5
   });
+}
+
+jQuery(document).ready(function(){
+    setTimeout('add_shadows()', 1000);
 });

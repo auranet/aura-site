7cd2ac525f2fdaab23a6d14d5eb8d097e0a4c3be
Tyler Lesmann <redhatcat@gmail.com>
2010-09-08 12:31:46 -0600

    Cap task for managing aura config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,11 @@ role :web, "www.aura-software.com"
 role :app, "www.aura-software.com"
 role :db,  "www.aura-software.com", :primary => true
 set :git_enable_submodules, 1
+
+namespace :aura do
+  task :link_config do
+    run "ln -sf #{shared_path}/config/aura.yml #{release_path}/config/aura.yml"
+  end
+end
+
+after "deploy:finalize_update", "aura:link_config"
